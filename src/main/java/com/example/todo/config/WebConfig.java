55ca4d5fd3753fda4d55af9c32bf9f2134c99208
Harold Muyba <hmuyba@gmail.com>
2024-12-30 10:49:28 -0400

    adding netlify to cors

--- src/main/java/com/example/todo/config/WebConfig.java
+++ src/main/java/com/example/todo/config/WebConfig.java
@@ -14,7 +14,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200", "https://todo-frontend-gamma-lake.vercel.app")
+                .allowedOrigins("http://localhost:4200", "https://todo-frontend-gamma-lake.vercel.app", "https://6772b2354e30748cf4e3330b--coruscating-klepon-09331a.netlify.app")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .exposedHeaders("Cache-Control", "Content-Type")
